50dfe412eda8e3c3caae198a4dfdb6cb594e1486
melodytowett <melodytowett99@gmail.com>
2022-01-19 23:04:57 +0300

    doing the user interface and business logic

--- page/script.js
+++ page/script.js
@@ -1,33 +1,59 @@
-$(document).ready(function() {
-    $("#btn").click(function() {
-      $(".myForm").toggle();
-    });
-    $("#btn1").click(function() {
-        $(".myForm1").toggle();
-      });
-      $("#btn2").click(function() {
-        $(".myForm2").toggle();
-      });
-      $("#btn3").click(function() {
-        $(".myForm3").toggle();
-      });
-      $("#btn4").click(function() {
-        $(".myForm4").toggle();
-      });
-      $("#btn5").click(function() {
-        $(".myForm5").toggle();
-        // $("#show").show();
-      });
-  }); 
-  $(document).ready(function () {
-    $(".button").click(function (event) {
-        event.preventDefault();
-        var myName = $("#names").val();
-        var myNumber = $("#number").val();
-        var myEmail = $("#email").val();
-        var myMess = $("#mess").val();
-            $("#show").show();
-            $("#patient").html(myName);
-}) 
+
+    // function validateForm() {
+    //         var name = document.getElementById("names").value();
+    //         var email = document.getElementById("email").value();
+    //         var number = document.getElementById("number").value();
+    //         var doctor = document.getElementById("doctors").value();
+
+    //         if (name.value == "") {
+    //           alert("enter your name")
+    //           name.focus();
+    //           return false;
+    //         } if (email.value == "") {
+    //           alert("enter your email")
+    //           email.focus
+    //           return false;
+    //         } if (number.value == "") {
+    //           alert("enter your mobile number") 
+    //           number.focus
+    //           return false;
+    //         } if (doctor.value == "") {
+    //           alert("enter the specilist you want to visit")
+    //           doctor.focus
+    //           return false;
+    //         }
+    //         return true;
+    //       }
+  
+  function Booking(name, email, phoneNo, doctor, message) {
+  this.name = name;
+  this.email = email;
+  this.phoneNo = phoneNo;
+  this.doctor = doctor;
+  this.message = message;
+}
+$(document).ready(function () {
+
+  $("#btn").click(function (event) {
+    // event.preventDefault();
+   
+$("#form").show();
+
+$(".check").show();
+$("#btn").hide();
   })
+  $("#check").click(function (event) {
+    event.preventDefault()
+    var myName = $("#names").val();
+    var myEmail = $("#email").val();
+    var myPhoneNo = $("#number").val();
+    var myDoctor = $("#doctors").val();
+var myBooking = new Booking( myName, myEmail, myEmail, myPhoneNo, myDoctor)
+$(".show").show();
+$("#patient").html(myName);
+$("#doc").html(myDoctor);
+$("#form").hide();
+  })
+})    
+
 
