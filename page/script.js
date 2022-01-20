9437945c416b5a05f30eff3689786060e299f690
melodytowett <melodytowett99@gmail.com>
2022-01-20 09:45:01 +0300

    form validation and submittion

--- page/script.js
+++ page/script.js
@@ -1,29 +1,3 @@
-
-    // function validateForm() {
-    //         var name = document.getElementById("names").value();
-    //         var email = document.getElementById("email").value();
-    //         var number = document.getElementById("number").value();
-    //         var doctor = document.getElementById("doctors").value();
-
-    //         if (name.value == "") {
-    //           alert("enter your name")
-    //           name.focus();
-    //           return false;
-    //         } if (email.value == "") {
-    //           alert("enter your email")
-    //           email.focus
-    //           return false;
-    //         } if (number.value == "") {
-    //           alert("enter your mobile number") 
-    //           number.focus
-    //           return false;
-    //         } if (doctor.value == "") {
-    //           alert("enter the specilist you want to visit")
-    //           doctor.focus
-    //           return false;
-    //         }
-    //         return true;
-    //       }
   
   function Booking(name, email, phoneNo, doctor, message) {
   this.name = name;
@@ -33,27 +7,28 @@
   this.message = message;
 }
 $(document).ready(function () {
-
-  $("#btn").click(function (event) {
-    // event.preventDefault();
-   
+$("#btn").click(function (event) {
 $("#form").show();
-
 $(".check").show();
 $("#btn").hide();
+$(".show").hide();
   })
-  $("#check").click(function (event) {
+  $("#form").submit(function (event) {
     event.preventDefault()
     var myName = $("#names").val();
     var myEmail = $("#email").val();
     var myPhoneNo = $("#number").val();
     var myDoctor = $("#doctors").val();
 var myBooking = new Booking( myName, myEmail, myEmail, myPhoneNo, myDoctor)
-$(".show").show();
+//alert("thankyou we have recevied")
+$("#p").show();
 $("#patient").html(myName);
 $("#doc").html(myDoctor);
 $("#form").hide();
+$("#btn").show();
+$("#check").hide();
   })
-})    
+
+ })    
 
 
